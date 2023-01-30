0f6ca6748021d55c5d27c8f84fdfcfe16c726471
Lena Voytek <lena.voytek@canonical.com>
2023-01-29 22:08:56 -0700

    Add .env interpretation to server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { ConfigModule } from '@nestjs/config';
+import config from './config';
+
 import { RsvpController } from './rsvp/rsvp.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, RsvpController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			load: [config]
+		})
+	],
+	controllers: [AppController, RsvpController],
+  	providers: [AppService],
 })
 export class AppModule {}
