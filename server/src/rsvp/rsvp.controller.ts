540bda33254737048a4397db9ac92771fd05ade8
Lena Voytek <lena.voytek@canonical.com>
2023-01-29 21:45:22 -0700

    Create library type shortcut and fix spacing

--- server/src/rsvp/rsvp.controller.ts
+++ server/src/rsvp/rsvp.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import {rsvpData} from '../../../libs/types/person';
+import { rsvpData } from '@libs/person';
 
 @Controller('rsvp')
 export class RsvpController {
 	@Post('create')
-    async create(@Body() rsvp: rsvpData): Promise<any> {
-      return rsvp;
-    }
+	async create(@Body() rsvp: rsvpData): Promise<any> {
+		return rsvp;
+	}
 }
