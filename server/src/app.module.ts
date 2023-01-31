c2c949d5e0d00d53e5a07656eb65c0ade21d0c63
Lena Voytek <lena.voytek@canonical.com>
2023-01-30 17:37:08 -0700

    Setup and add config for mysql and sqlite dbs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,18 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from './config';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { RsvpController } from './rsvp/rsvp.controller';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
-			load: [config]
-		})
+			load: [config],
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				type: configService.get<'mysql' | 'sqlite'>('db.system'),
+				database: configService.get<string>('db.name'),
+				username: configService.get<string>('db.username'),
+				password: configService.get<string>('db.password'),
+				entities: [__dirname + '/**/*.entity{.ts,.js}'],
+				synchronize: true,
+			}),
+			inject: [ConfigService],
+		}),
 	],
 	controllers: [AppController, RsvpController],
-  	providers: [AppService],
+	providers: [AppService],
 })
 export class AppModule {}
--- server/src/config.ts
+++ server/src/config.ts
@@ -1,3 +1,9 @@
 export default () => ({
 	port: parseInt(process.env.PORT, 10) || 3000,
+	db: {
+		system: process.env.DB_TYPE ?? "sqlite",
+		name: process.env.DB_NAME ?? './db.sqlite3',
+		username: process.env.DB_USERNAME ?? 'root',
+		password: process.env.DB_PASSWORD ?? ''
+	}
 });
