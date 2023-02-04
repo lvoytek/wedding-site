4afe81971e29ac10c9b20e88a1d375d3eac31559
Lena Voytek <lena@voytek.dev>
2023-02-03 17:21:59 -0700

    Add assignments entity

---
+++ server/src/entities/assignment.entity.ts
@@ -0,0 +1,36 @@
+import {
+	Entity,
+	Column,
+	PrimaryGeneratedColumn,
+	OneToOne,
+	JoinColumn,
+} from 'typeorm';
+import { Guest } from './guest.entity';
+
+@Entity()
+export class Assignment {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	table: number;
+
+	@Column()
+	googleAuthId: string;
+
+	@Column()
+	isInGroomishParty: boolean;
+
+	@Column()
+	isInBridalParty: boolean;
+
+	@Column()
+	isFamily: boolean;
+
+	@Column()
+	pokemon: string;
+
+	@OneToOne(() => Guest)
+    @JoinColumn()
+    guest: Guest
+}
