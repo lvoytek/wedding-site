38964e355337880004f2c6ed2f663e47ca0821c4
Lena Voytek <lena@voytek.dev>
2023-02-03 18:08:38 -0700

    Add Plus One and Associate entities

---
+++ server/src/entities/associate.ts
@@ -0,0 +1,21 @@
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	OneToOne,
+	JoinColumn,
+} from 'typeorm';
+import { Guest } from './guest.entity';
+
+@Entity()
+export class Associate {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@OneToOne(() => Guest)
+    @JoinColumn()
+    guestOne: Guest
+
+	@OneToOne(() => Guest)
+    @JoinColumn()
+    guestTwo: Guest
+}
---
+++ server/src/entities/plusone.entity.ts
@@ -0,0 +1,21 @@
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	OneToOne,
+	JoinColumn,
+} from 'typeorm';
+import { Guest } from './guest.entity';
+
+@Entity()
+export class PlusOne {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@OneToOne(() => Guest)
+    @JoinColumn()
+    guest: Guest
+
+	@OneToOne(() => Guest)
+    @JoinColumn()
+    plusOne: Guest
+}
