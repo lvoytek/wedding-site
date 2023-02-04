21f0dd502240f3c52919d5600b154e4f2f84fb90
Lena Voytek <lena@voytek.dev>
2023-02-03 20:24:50 -0700

    Fix extra / in urls

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	SERVER_URL: 'https://l2.wedding/'
+	SERVER_URL: 'https://l2.wedding'
  };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: false,
-	SERVER_URL: 'http://localhost:3003/'
+	SERVER_URL: 'http://localhost:3003'
 };
