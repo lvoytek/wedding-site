2e4158608cfaa99f70a1f047b82a60072846221c
Lena Voytek <lena@voytek.dev>
2023-03-24 22:36:04 -0700

    Move pokemon validation to its own function

--- client/src/app/components/rsvp/rsvp.component.ts
+++ client/src/app/components/rsvp/rsvp.component.ts
@@ -1,35 +1,52 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { submissionData, guestData } from "@libs/person";
-import { RecursivePartial } from "@libs/utils";
-import { RsvpService } from "../../services/rsvp.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { submissionData, guestData } from '@libs/person';
+import { RecursivePartial } from '@libs/utils';
+import { RsvpService } from '../../services/rsvp.service';
 
 @Component({
-  selector: 'app-rsvp',
-  templateUrl: './rsvp.component.html',
-  styleUrls: ['./rsvp.component.scss']
+	selector: 'app-rsvp',
+	templateUrl: './rsvp.component.html',
+	styleUrls: ['./rsvp.component.scss'],
 })
 export class RsvpComponent implements OnInit {
-	pokemon: string = "";
+	pokemon: string = '';
+	pokemonValidated: boolean = false;
 	existingRsvpData: RecursivePartial<guestData> = {};
 
-	constructor(private api: RsvpService, private router: Router, private route: ActivatedRoute){}
+	constructor(
+		private api: RsvpService,
+		private router: Router,
+		private route: ActivatedRoute
+	) {}
 
 	ngOnInit() {
 		const pokemonInput = this.route.snapshot.queryParamMap.get('code');
-		if(pokemonInput) {
-			this.pokemon = pokemonInput;
-			this.api.getRSVPFromCode(this.pokemon).subscribe((data: any) => {
-				this.existingRsvpData = data;
+		if (pokemonInput) this.validatePokemon(pokemonInput);
+	}
+
+	/**
+	 * Check if a pokemon input is valid from the server, if so set the pokemon and mark as valid
+	 * @param pokemon The pokemon name input to check
+	 */
+	validatePokemon(pokemon: string) {
+		if (pokemon) {
+			this.api.getRSVPFromCode(pokemon).subscribe((data: any) => {
+				this.pokemonValidated = false;
+
+				if (data) {
+					this.pokemonValidated = true;
+					this.pokemon = pokemon;
+					this.existingRsvpData = data;
+				}
 			});
 		}
 	}
 
-	sendForm(rsvpData: RecursivePartial<submissionData>){
+	sendForm(rsvpData: RecursivePartial<submissionData>) {
 		//TODO: Not sure what to actually do after sending for now
 		this.api.sendRSVP(rsvpData).subscribe(() => {
 			this.router.navigate(['']);
-		})
+		});
 	}
-
 }
