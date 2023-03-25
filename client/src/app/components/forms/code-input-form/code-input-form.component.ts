21b182ed6ada5e83fd7d207f1442987ebe3b9866
Lena Voytek <lena@voytek.dev>
2023-03-24 22:21:41 -0700

    Create code input form

--- client/src/app/components/forms/code-input-form/code-input-form.component.ts
+++ client/src/app/components/forms/code-input-form/code-input-form.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-code-input-form',
 	templateUrl: './code-input-form.component.html',
 	styleUrls: ['./code-input-form.component.scss'],
 })
-export class CodeInputFormComponent {}
+export class CodeInputFormComponent {
+	@Output() submit = new EventEmitter<string>();
+
+	codeForm = this.fb.group({
+		code: ['', Validators.required],
+	});
+
+	constructor(private fb: NonNullableFormBuilder) {}
+
+	onSubmit() {
+		// do something with the code
+		console.log(this.codeForm.value.code);
+	}
+}
