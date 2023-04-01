310303439f21d866a040992eedf0010bf7a63896
Lena Voytek <lena@voytek.dev>
2023-04-01 15:45:04 -0700

    Add initial admin table setup

--- client/src/app/components/forms/admin-table/admin-table.component.ts
+++ client/src/app/components/forms/admin-table/admin-table.component.ts
@@ -1,31 +1,74 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { NonNullableFormBuilder, Validators} from '@angular/forms';
-import { primaryData } from '@libs/person';
-import { RecursivePartial } from '@libs/utils';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { guestData } from '@libs/person';
+
+const ELEMENT_DATA: guestData[] = [
+	{
+		uuid: '123',
+		firstName: 'Test',
+		lastName: 'User',
+		email: 'test@gmail.com',
+		isGoing: true,
+		table: 1,
+		isInBridalParty: false,
+		isInGroomishParty: false,
+		isFamily: false,
+		pokemon: 'Pikachu',
+	},
+	{
+		uuid: '456',
+		firstName: 'Best',
+		lastName: 'User',
+		email: 'test@gmail.com',
+		isGoing: true,
+		table: 1,
+		isInBridalParty: false,
+		isInGroomishParty: false,
+		isFamily: false,
+		pokemon: 'Pichu',
+	},
+	{
+		uuid: '789',
+		firstName: 'Last',
+		lastName: 'User',
+		email: 'test@gmail.com',
+		isGoing: true,
+		table: 1,
+		isInBridalParty: false,
+		isInGroomishParty: false,
+		isFamily: false,
+		pokemon: 'Raichu',
+	},
+];
 
 @Component({
-  selector: 'app-admin-table',
-  templateUrl: './admin-table.component.html',
-  styleUrls: ['../form.component.scss']
+	selector: 'app-admin-table',
+	templateUrl: './admin-table.component.html',
+	styleUrls: ['../form.component.scss'],
 })
-export class AdminTableComponent {
-	@Output() submit = new EventEmitter<RecursivePartial<primaryData>>()
+export class AdminTableComponent implements OnInit {
+	displayedColumns: string[] = [
+		'uuid',
+		'firstName',
+		'lastName',
+		'email',
+		'isGoing',
+		'actions',
+	];
+	dataSource = ELEMENT_DATA;
 
-	guestForm = this.fb.group({
-		firstName: ['', Validators.required],
-		lastName: ['', Validators.required]
-	});
+	constructor() {}
 
-	constructor(private fb: NonNullableFormBuilder){}
+	ngOnInit(): void {}
 
-	onSubmit(){
-		const guestData: RecursivePartial<primaryData> = {
-			firstName: this.guestForm.value.firstName,
-			lastName: this.guestForm.value.lastName,
-		}
-
-		this.submit.emit(guestData);
-		this.guestForm.reset();
+	onEdit(element: guestData) {
+		// Handle edit operation here
 	}
 
+	onDelete(element: guestData) {
+		// Handle delete operation here
+	}
+
+	onSave(element: guestData) {
+		// Handle save operation here
+	}
 }
--- client/src/app/material.module.ts
+++ client/src/app/material.module.ts
@@ -7,26 +7,30 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-	MatFormFieldModule,
-	MatSelectModule,
-	MatInputModule,
-	MatCheckboxModule
-  ],
-  exports: [
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-	MatFormFieldModule,
-	MatSelectModule,
-	MatInputModule,
-	MatCheckboxModule
-  ]
+	declarations: [],
+	imports: [
+		CommonModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatFormFieldModule,
+		MatSelectModule,
+		MatInputModule,
+		MatCheckboxModule,
+	],
+	exports: [
+		MatButtonModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatFormFieldModule,
+		MatSelectModule,
+		MatInputModule,
+		MatCheckboxModule,
+		MatTableModule,
+		MatGridListModule,
+	],
 })
-export class MaterialModule { }
+export class MaterialModule {}
