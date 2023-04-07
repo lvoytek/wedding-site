7d15acdff8b0690da7b11c322582e3dc9515d42c
Lena Voytek <lena@voytek.dev>
2023-04-06 20:05:20 -0700

    Create admin page nav button when admin logged in

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,21 +1,35 @@
 import { SocialAuthService } from '@abacritt/angularx-social-login';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { GuestService } from './services/guest.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 	title = 'Wedding Site';
+	isAdminLoggedIn = false;
 
-	constructor(private googleService: SocialAuthService, private authService: AuthService){}
+	constructor(
+		private googleService: SocialAuthService,
+		private authService: AuthService,
+		private guestService: GuestService
+	) {}
 
 	ngOnInit() {
 		this.googleService.authState.subscribe((user) => {
 			const token: string = user?.idToken || '';
 			this.authService.login(token);
 		});
-	  }
+
+		this.authService.isLoggedIn.subscribe((isLoggedIn) => {
+			if (isLoggedIn) {
+				this.guestService.isAdmin().subscribe((data: any) => {
+					this.isAdminLoggedIn = data;
+				});
+			}
+		});
+	}
 }
